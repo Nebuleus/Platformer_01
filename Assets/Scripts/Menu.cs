dd1ab53fddc123257e326479268e2a603f27d219
Alice Bleuse <bleusealice1@gmail.com>
2020-10-23 16:55:11 +0200

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame()
+ {
+     SceneManager.LoadScene("Map");
+ }
+    public void QuitGame()
+ {
+     Application.Quit();
+      Debug.Log("EXIT GAME");
+ }
+     public void Credits()
+    {
+        SceneManager.LoadScene("Credits");    //permet de charger la scene "Credits"
+    }
+    public void Leave()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
